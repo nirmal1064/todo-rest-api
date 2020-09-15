f59dc6f2f70e670f00ee1c1d0d532d06f2f2a272
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-16 00:23:23 +0530

    modified authentication strategy

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
 
 const register = (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+        return;
+    }
+
     const hashedPassword = bcrypt.hashSync(req.body.password, 8);
     const user = new User({
         name: req.body.name,
-        email : req.body.email,
+        username : req.body.username,
         password : hashedPassword
     });
     user.save((err, result) => {
-        if (err) return res.status(500).send("Problem in registering the user");
-        const token = jwt.sign({ id: result.id }, process.env.SECRET_KEY, {
-            expiresIn: 86400
-        });
-        res.status(200).send({ auth: true, token: token });
+        if (err) return res.status(500).send({ message: "Problem in registering the user " + err });
+        res.status(200).send(result);
     });
 }
 
 const login = (req, res) => {
-    User.findOne({ email: req.body.email }, (err, result) => {
-        if (err) return res.status(500).send({ message: "Internal Server Error" });
+    User.findOne({ username: req.body.username }, (err, result) => {
+        if (err) return res.status(500).send({ message: "Internal Server Error " + err });
         if (!result) return res.status(404).send({ message: "User Not found" });
         const passwordIsValid = bcrypt.compareSync(req.body.password, result.password);
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
@@ -28,7 +33,7 @@ const login = (req, res) => {
         const token = jwt.sign({ id: result.id }, process.env.SECRET_KEY, {
             expiresIn: 86400
         });
-        res.status(200).send({ auth: true, token: token });
+        res.status(200).send({ auth: true, token: "Bearer " + token });
     });
 }
 
--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const verifyToken = require('../middlewares/verifyToken');
+const signupValidator = require('../middlewares/validator');
 
-router.post("/register", userController.register);
+router.post("/register", signupValidator(), userController.register);
 router.post("/login", userController.login);
 router.get("/home", verifyToken, userController.home);
 router.get("/logout", userController.logout);
