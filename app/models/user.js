46e998b97742223f51f794d7cde6a5b0ce077bf3
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-16 00:20:14 +0530

    modified user schema

--- app/models/user.js
+++ app/models/user.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 },{ timestamps: true });
 
 UserSchema.method("toJSON", function() {
