273da58934982c5b2db27a74075a6f6f94fff982
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-16 00:18:50 +0530

    mongoose removing depracated modules

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const app = require('./server');
 const port = process.env.PORT || 8082;
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const logger = require('morgan');
 if (app.get('env') === 'development') {
     require('dotenv').config();
     app.use(logger('dev'));
 }
-mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true, 
+    useFindAndModify: false, useCreateIndex: true });
 const db = mongoose.connection;
 db.once("open", (_) => {
     console.log("db connection established");
