d4d57d29bfe2cf1462b747aa46406d1a3b7c9029
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-16 00:19:22 +0530

    passport middleware

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 const app = express();
 const todoRouter = require('./app/routes/todoRouter');
 const userRouter = require('./app/routes/userRouter');
+const passport = require('passport');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/todo', todoRouter);
 app.use('/api', userRouter);
+app.use(passport.initialize());
+require('./app/config/passport');
 app.get("/", (req, res) => {
     res.json({ message: "Welcome" });
 });
