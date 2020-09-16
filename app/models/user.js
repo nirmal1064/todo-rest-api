5d73f89a15fe0c219a2f9e96741d2193952cfea4
Nirmal Kumar <nirmal1064@gmail.com>
2020-09-17 00:05:44 +0530

    changed quotes

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -17,10 +17,10 @@ const UserSchema = new Schema({
     }
 },{ timestamps: true });
 
-UserSchema.method("toJSON", function() {
+UserSchema.method('toJSON', function() {
     const { __v, _id, ...object } = this.toObject();
     object.id = _id;
     return object;
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
